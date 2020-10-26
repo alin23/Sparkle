9f55cb29c8ca131906bcc0fe12e8b1996001d2a7
Alin Panaitiu <alin.panaitiu@gmail.com>
2020-10-26 13:02:33 +0200

    Package.swift

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let version = "1.24.0"
 let checksum = "2ac9b595d822343a58bcc2818af2be3ddac147683430a1cdae90343fa15f4523"
-let url = "https://github.com/sparkle-project/Sparkle/releases/download/\(version)/Sparkle-SPM-\(version).zip"
+let url = "https://github.com/alin23/Sparkle/releases/download/\(version)/Sparkle-SPM-\(version).zip"
 
 let package = Package(
     name: "Sparkle",
